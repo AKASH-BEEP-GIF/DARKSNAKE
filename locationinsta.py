edde0c28d197e3265ff029c9543422d366db38f9
Agash <aakak6769@gmail.com>
2026-09-20 23:42:22 +0530

    Update app.run configuration in locationinsta.py
    Disable debug mode and reloader in app.run.

--- locationinsta.py
+++ locationinsta.py
@@ -184,7 +184,8 @@ def front():
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    host="0.0.0.0",
+    port=5000,
+    debug=False,
+    use_reloader=False
+)
